5bc763fea7273ec7b104f25eaaf93369998045be
Ousssama El-Amrani <oussama.maroc8@gmail.com>
2025-12-28 01:15:49 +0100

    feat: init project with their dependencies

---
+++ src/main/java/me/elamranioussama/bankaccountgraphqlservice/BankAccountGraphQlServiceApplication.java
@@ -0,0 +1,13 @@
+package me.elamranioussama.bankaccountgraphqlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccountGraphQlServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccountGraphQlServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/me/elamranioussama/bankaccountgraphqlservice/BankAccountGraphQlServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.elamranioussama.bankaccountgraphqlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountGraphQlServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
